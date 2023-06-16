0eb3332f23d6dafbbad3104838f0085fdb9e859e
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-16 09:47:05 +0300

    Add Entities

---
+++ ItemMarketplace/Data/Entities/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Item : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string MetaData { get; set; }
+        public virtual List<Sale> Sales { get; set; }
+
+    }
+}
---
+++ ItemMarketplace/Data/Entities/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Sale: BaseEntity
+    {
+        public int ItemId { get; set; }
+        public virtual Item Item { get; set; }
+        public DateTime CreatedDt { get; set; }
+        public DateTime FinishedDt { get; set; }
+        public decimal Price { get; set; }
+        public int StatusId { get; set; }
+        public virtual MarketStatus Status { get; set; }
+        public string Seller { get; set; }
+        public string Buyer { get; set; }
+    }
+}
