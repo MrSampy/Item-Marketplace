d2427d2378a0e4b7d7c10b4ec2dd132e3436839d
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-16 09:47:21 +0300

    Add ItemMarketplaceDBContext

---
+++ ItemMarketplace/Data/Data/ItemMarketplaceDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using System.Data;
+
+namespace Data.Data
+{
+    public class ItemMarketplaceDBContext : DbContext
+    {
+        public DbSet<Item> Item { get; set; }
+        public DbSet<MarketStatus> MarketStatus { get; set; }
+        public DbSet<Sale> Sale { get; set; }
+        public ItemMarketplaceDBContext(DbContextOptions<ItemMarketplaceDBContext> options) : base(options)
+        { }
+
+    }
+}
