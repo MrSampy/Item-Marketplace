0d6588d46a2ccca84dad1c0b5f08cc4c3975dfe1
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-16 09:47:29 +0300

    Add UnitOfWork

---
+++ ItemMarketplace/Data/Data/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Repositories;
+namespace Data.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ItemMarketplaceDBContext dbContext;
+
+        private MarketStatusRepository marketStatusRepository;
+        private ItemRepository itemRepository;
+        private SaleRepository saleRepository;
+
+        public IRepository<MarketStatus> MarketStatusRepository => marketStatusRepository ??= new MarketStatusRepository(dbContext);
+        public IRepository<Item> ItemRepository => itemRepository ??= new ItemRepository(dbContext);
+        public IRepository<Sale> SaleRepository => saleRepository ??= new SaleRepository(dbContext);
+        public UnitOfWork(ItemMarketplaceDBContext dBContext)
+        {
+            this.dbContext = dBContext;
+        }
+
+    }
+}
