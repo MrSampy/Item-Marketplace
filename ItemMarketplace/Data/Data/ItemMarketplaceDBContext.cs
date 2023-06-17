f623be381a71b72164372f7fd36866f67519188c
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-17 22:47:50 +0300

    Add User-related entities to ItemMarketplaceDBContext and configure relationships

--- ItemMarketplace/Data/Data/ItemMarketplaceDBContext.cs
+++ ItemMarketplace/Data/Data/ItemMarketplaceDBContext.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Data.Entities;
 using System.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace Data.Data
 {
@@ -14,8 +15,29 @@ namespace Data.Data
         public DbSet<Item> Item { get; set; }
         public DbSet<MarketStatus> MarketStatus { get; set; }
         public DbSet<Sale> Sale { get; set; }
-        public ItemMarketplaceDBContext(DbContextOptions<ItemMarketplaceDBContext> options) : base(options)
-        { }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserCredentials> UserCredentials { get; set; }
 
+        public ItemMarketplaceDBContext(DbContextOptions<ItemMarketplaceDBContext> options) : base(options)
+        {}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Seller)
+                .WithMany(u => u.SellerSales)
+                .HasForeignKey(s => s.SellerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Buyer)
+                .WithMany(u => u.BuyerSales)
+                .HasForeignKey(s => s.BuyerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+               .HasOne(u => u.UserCredentials)
+               .WithOne(uc => uc.User)
+               .HasForeignKey<UserCredentials>(uc => uc.UserId);
+        }
     }
 }
