4d5e2e824df8372ce57d465e4a5edd1808093ded
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-17 22:43:21 +0300

    Add UserCredentials

---
+++ ItemMarketplace/Data/Entities/UserCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class UserCredentials: BaseEntity
+    {
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
