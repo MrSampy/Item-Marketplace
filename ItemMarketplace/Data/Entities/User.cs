59bca9e26cbbda7e809772819b9260aa04b9a51f
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-17 22:42:46 +0300

    Add user enity

---
+++ ItemMarketplace/Data/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class User: BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+        public virtual ICollection<Sale> SellerSales { get; set; }
+        public virtual ICollection<Sale> BuyerSales { get; set; }
+        public int UserCredentialsId { get; set; }
+        public virtual UserCredentials UserCredentials { get; set; }
+    }
+}
