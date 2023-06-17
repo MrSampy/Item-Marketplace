2948ab4fd30f8dfb46262fa6c8b5d0c4951d174f
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-17 22:51:55 +0300

    Add ItemMarketplaceException

---
+++ ItemMarketplace/Business/Validation/ItemMarketplaceException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validation
+{
+    [Serializable()]
+    public class ItemMarketplaceException : Exception
+    {
+        public ItemMarketplaceException() { }
+        public ItemMarketplaceException(string message) : base(message) { }
+        public ItemMarketplaceException(string message, Exception inner) : base(message, inner) { }
+        protected ItemMarketplaceException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+    }
+}
