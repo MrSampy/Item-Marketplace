2d5edc47955afb71f15e6c10c99ebf77044af796
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-17 22:50:35 +0300

    Add UserRepository

---
+++ ItemMarketplace/Data/Repositories/UserRepository.cs
@@ -0,0 +1,81 @@
+﻿using Data.Data;
+using Data.Entities;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly ItemMarketplaceDBContext Context;
+        public UserRepository(ItemMarketplaceDBContext context)
+        {
+            Context = context;
+        }
+
+        public async Task AddAsync(User entity)
+        {
+            await Context.User.AddAsync(entity);
+
+            Context.SaveChanges();
+        }
+
+        public void Delete(User entity)
+        {
+            Context.User.Remove(entity);
+
+            Context.SaveChanges();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            Context.User.Remove(await GetByIdAsync(id));
+
+            Context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await Context.User.ToListAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllWithDetailsAsync()
+        {
+            return await Context.User
+                .Include(x => x.UserCredentials)
+                .Include(x=>x.SellerSales)
+                .Include(x=>x.BuyerSales)
+                .ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await Context.User.FirstOrDefaultAsync(x => x.Id.Equals(id));
+        }
+
+        public async Task<User> GetByIdWithDetailsAsync(int id)
+        {
+            return await Context.User
+                .Include(x => x.UserCredentials)
+                .Include(x => x.SellerSales)
+                .Include(x=> x.BuyerSales)
+                .FirstOrDefaultAsync(x => x.Id.Equals(id));
+        }
+
+        public void Update(User entity)
+        {
+            var entityToUpdate = Context.User.First(x => x.Id.Equals(entity.Id));
+
+            entityToUpdate.Name = entity.Name;
+            entityToUpdate.Surname = entity.Surname;
+            entityToUpdate.EmailAddress = entity.EmailAddress;
+            entityToUpdate.UserCredentialsId = entity.UserCredentialsId;
+            Context.SaveChanges();
+        }
+    }
+}
