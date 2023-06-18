5027531179cacbe886276c2d616b59d106a07ddc
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-18 16:26:40 +0300

    Add IUserService

---
+++ ItemMarketplace/Business/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IUserService: ICrud<UserModel>
+    {       
+        public Task<UserModel> GetUserByCredentials(string nickName, string password);
+        public Task AddUserCredentials(UserCredentialsModel model);
+        public Task DeleteUserCredentials(int id);
+        public Task UpdateUserCredentials(UserCredentialsModel model);
+
+    }
+}
