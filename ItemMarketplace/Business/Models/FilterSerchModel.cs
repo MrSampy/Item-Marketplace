7f720e8d23a1c5dd0e69e68ca501ec943e619676
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-18 16:24:57 +0300

    Add FilterSerchModel

---
+++ ItemMarketplace/Business/Models/FilterSerchModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class FilterSerchModel
+    {
+        public string? Name { get; set; }
+        public string? Status { get; set; }
+        public string? SortOrder { get; set; }
+        public string? SortKey { get; set; }
+        public int? Limit { get; set; }
+
+    }
+}
