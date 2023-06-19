1c934a94b01830aaefd0d55b80dc9104e3f69d5e
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-20 00:05:53 +0300

    Add Startup

---
+++ ItemMarketplace/API/Startup.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using Data.Data;
+using Microsoft.EntityFrameworkCore;
+using Business.Validation;
+using Data.Interfaces;
+using Business.Interfaces;
+using Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection")!;
+            var optionsBuilder = new DbContextOptionsBuilder<ItemMarketplaceDBContext>();
+
+            var options = optionsBuilder
+                    .UseSqlServer(connection)
+                    .Options;
+            var context = new ItemMarketplaceDBContext(options);
+            var unitOfWork = new UnitOfWork(context);
+
+            services.AddMemoryCache();
+
+            services.AddControllers();
+
+            services.AddDbContext<ItemMarketplaceDBContext>(options => options.UseSqlServer(connection));
+
+            services.AddAutoMapper(typeof(AutomapperProfile).Assembly);
+
+            services.AddSingleton<IUnitOfWork>(x => unitOfWork);
+
+            services.AddTransient<IUserService, UserService>();
+
+            services.AddTransient<IAuctionsService, AuctionsService>();
+
+            services.AddTransient<IAuthService, AuthService>();
+            
+            services.AddTransient<ICacheService, CacheService>();
+
+            #region Swagger Configuration
+
+
+            services.AddSwaggerGen(swagger =>
+            {
+                // To Enable authorization using Swagger (JWT)
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+            #endregion
+            #region Authentication
+            services.AddAuthorization(options =>
+            {
+                var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(
+                    JwtBearerDefaults.AuthenticationScheme);
+
+                defaultAuthorizationPolicyBuilder =
+                    defaultAuthorizationPolicyBuilder.RequireAuthenticatedUser();
+
+                options.DefaultPolicy = defaultAuthorizationPolicyBuilder.Build();
+            });
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+            #endregion
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+        }
+    }
+}
