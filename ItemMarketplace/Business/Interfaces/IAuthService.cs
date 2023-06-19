9b34b052d118ffe5135477c5178e301e8b8918e9
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-20 00:00:18 +0300

    Add IAuthService

---
+++ ItemMarketplace/Business/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<UserModel> GetUserByCredentials(string nickName, string password);
+
+    }
+}
