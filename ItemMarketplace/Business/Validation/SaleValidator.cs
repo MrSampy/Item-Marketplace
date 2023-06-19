10c26b1b543ed4689f35c9a8dd42f99304a63838
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-19 23:59:26 +0300

    Refactor SaleValidator class for validation in Add, Update, and Id scenarios

--- ItemMarketplace/Business/Validation/SaleValidator.cs
+++ ItemMarketplace/Business/Validation/SaleValidator.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Interfaces;
+using Data.Entities;
 
 namespace Business.Validation
 {
@@ -16,37 +17,56 @@ namespace Business.Validation
         {
             this.unitOfWork = unitOfWork;
         }
-        public async Task<ValidationResult> ValidateAsync(SaleModel sale)
+
+        public async Task<ValidationResult> ValidateForAddAsync(SaleModel model)
         {
             var result = new ValidationResult()
             {
                 IsValid = true,
                 Messages = new List<string>()
             };
-            var item = await unitOfWork.ItemRepository.GetByIdAsync(sale.ItemId);
+            var item = await unitOfWork.ItemRepository.GetByIdAsync(model.ItemId);
             if (item == null)
             {
                 result.IsValid = false;
                 result.Messages.Add("Item not found");
             }
-            var status = await unitOfWork.MarketStatusRepository.GetByIdAsync(sale.StatusId);
+            var status = await unitOfWork.MarketStatusRepository.GetByIdAsync(model.StatusId);
             if (status == null)
             {
                 result.IsValid = false;
                 result.Messages.Add("Status not found");
             }
-            var seller = await unitOfWork.UserRepository.GetByIdAsync(sale.SellerId);
+            var seller = await unitOfWork.UserRepository.GetByIdAsync(model.SellerId);
             if (seller == null)
             {
                 result.IsValid = false;
                 result.Messages.Add("Seller not found");
             }
-            var buyer = await unitOfWork.UserRepository.GetByIdAsync(sale.BuyerId);
+            var buyer = await unitOfWork.UserRepository.GetByIdAsync(model.BuyerId);
             if (buyer == null)
             {
                 result.IsValid = false;
                 result.Messages.Add("Buyer not found");
             }
+            
+            var validation = ValidateAsync(model);
+
+            return new ValidationResult 
+            {
+                IsValid = result.IsValid && validation.IsValid,
+                Messages = result.Messages.Concat(validation.Messages).ToList()
+            };
+        }
+
+
+        public ValidationResult ValidateAsync(SaleModel sale)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
             if (sale.Price <= 0)
             {
                 result.IsValid = false;
@@ -62,6 +82,11 @@ namespace Business.Validation
                 result.IsValid = false;
                 result.Messages.Add("Finished date must be greater than created date");
             }
+            if (sale.FinishedDt > DateTime.Today)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Finished date must be less than today");
+            }
 
             return result;
         }
@@ -69,7 +94,7 @@ namespace Business.Validation
         public async Task<ValidationResult> ValidateForUpdateAsync(SaleModel model)
         {
             var validationIdResult = await ValidateIdAsync(model.Id);
-            var validationResult = await ValidateAsync(model);
+            var validationResult = ValidateAsync(model);
 
             return new ValidationResult()
             {
